fcc97d46bb9981e12e03d5036c49efdb39697e53
ilia_programmer <ilia20221401@gmail.com>
2026-09-22 13:20:11 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,8 @@ pluginManagement {
   }
 }
 
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" 
+          id("com.google.devtools.ksp") version "2.0.21-1.0.25"}
 
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
