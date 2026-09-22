b02dd879355b7e4bf409007fe9c5d487cbc0c43c
ilia_programmer <ilia20221401@gmail.com>
2026-09-22 13:21:10 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,6 @@ plugins {
   alias(libs.plugins.roborazzi) apply false
   alias(libs.plugins.secrets) apply false
   alias(libs.plugins.google.services) apply false
+  id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+  id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
 }
